97208e7cbd0f06dfe7c0e4b4d50abeed7dbd652b
Booyamando <filibs10@gmail.com>
2020-12-08 21:23:43 +0000

    first commit

--- src/main.js
+++ src/main.js
@@ -6,5 +6,16 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  let values = ["A", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  let suites = ["heart", "club", "spade", "diamond"];
+
+  let getRndInteger = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+
+  document
+    .querySelector(".card")
+    .classList.add(suites[getRndInteger(0, suites.length - 1)]);
+  document.querySelector(".text").innerHTML =
+    values[getRndInteger(0, values.length - 1)];
 };
